a6bb3c9d2fe0582237044392e14e56ba707c9f77
= <=>
2025-05-02 10:11:41 +0200

    chore: eliminar archivos de base de datos temporales y actualizar información de ensamblado

--- obj/Debug/net8.0/MyBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb7dbdd93c93139882d74c933449f5f2b1c73c11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb5427bc3780d122dd68d455b4156f263e6a3559")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
