756a8b9ac17b65ac1105adfa2086f8794d093cd2
= <=>
2025-05-10 17:32:22 +0200

    chore: update build artifacts and assembly info
    This commit includes updates to the build artifacts and assembly information files, reflecting changes in the build process and versioning. The modifications are part of routine maintenance and do not introduce functional changes to the codebase.

--- obj/Debug/net8.0/MyBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e052a588849e1029a4758d1d79056f233eacdf6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c42673bab69ff0eee4723de362f7fba33d61fcf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
